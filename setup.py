794217ce4066f1f1fb65fa0de9b2acf1351933b2
Ayush Nayal <aynayal@gmail.com>
2024-08-03 11:35:58 +0530

    Initializing repository with DVC implementation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Testing Data Version Control using DVC & GitHub.',
+    author='Ayush',
+    license='',
+)
